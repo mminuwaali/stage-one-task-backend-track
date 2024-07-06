d394e4cdfa25f1c6171e2f42102e34e7b571f2fd
Muhammad Muhamamd Inuwa <mminuwaali@gmail.com>
2024-07-07 00:14:21 +0100

    Added `@Req()` parameter to `getHello` method in `AppController`.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
 import { AppService } from './app.service';
-import { Controller, Get, Query, Ip } from '@nestjs/common';
+import { Controller, Get, Query, Ip, Req } from '@nestjs/common';
 
 @Controller("api")
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get("hello")
-  async getHello(@Query("visitor_name") visitorName: string, @Ip() ip: string) {
-    console.log(ip, "client ip address"); // @ts-ignore
+  async getHello(@Query("visitor_name") visitorName: string, @Ip() ip: string, @Req() request: Request) { // @ts-ignore
+    console.log(request.clientIp || ip, "client ip address"); // @ts-ignore
     return this.appService.getHello(request.clientIp || ip, visitorName);
   }
 }
